9b2df2ea39e130f66b0e06630b1a61bcfea909f3
Alex Curran <aml.curran@gmail.com>
2014-11-09 21:41:59 +0000

    Add icons to start activity

--- app/src/main/java/uk/co/amlcurran/lpreviewdemo/DefaultThemeWidgetsActivity.java
+++ app/src/main/java/uk/co/amlcurran/lpreviewdemo/DefaultThemeWidgetsActivity.java
@@ -1,10 +1,7 @@
 package uk.co.amlcurran.lpreviewdemo;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class DefaultThemeWidgetsActivity extends Activity {
@@ -15,31 +12,4 @@ public class DefaultThemeWidgetsActivity extends Activity {
         setContentView(R.layout.activity_widgets);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.default_theme, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_themed) {
-            startActivity(new Intent(this, CustomThemeWidgetsActivity.class));
-            return true;
-        }
-        else if (id == R.id.action_transitions) {
-            startActivity(new Intent(this, TransitionEntryActivity.class));
-            return true;
-        } else if (id == R.id.action_cards) {
-            startActivity(new Intent(this, CardViewActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/uk/co/amlcurran/lpreviewdemo/StartActivity.java
+++ app/src/main/java/uk/co/amlcurran/lpreviewdemo/StartActivity.java
@@ -3,17 +3,26 @@ package uk.co.amlcurran.lpreviewdemo;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class StartActivity extends Activity {
 
     private static final String[] SELECTION_ITEMS = new String[] {
             "Widgets", "Themed widgets", "Transitions", "Circular reveal", "CardView"
     };
+    private static final int[] SELECTION_ICONS = new int[] {
+            R.drawable.ic_widgets,
+            R.drawable.ic_create,
+            R.drawable.ic_input,
+            R.drawable.ic_circular,
+            R.drawable.ic_cards };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +36,26 @@ public class StartActivity extends Activity {
 
     private class FixedAdapter extends ArrayAdapter<String> {
 
+        private final int drawablePadding;
+
         public FixedAdapter(Context context) {
             super(context, android.R.layout.simple_list_item_1);
             addAll(SELECTION_ITEMS);
+            drawablePadding = getResources().getDimensionPixelOffset(R.dimen.padding);
         }
 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = (TextView) super.getView(position, convertView, parent);
+            int selectionIconId = SELECTION_ICONS[position];
+            if (selectionIconId != 0) {
+                Drawable image = getResources().getDrawable(selectionIconId);
+                image.setTint(getResources().getColor(R.color.primary));
+                textView.setCompoundDrawablePadding(drawablePadding);
+                textView.setCompoundDrawablesWithIntrinsicBounds(image, null, null, null);
+            }
+            return textView;
+        }
     }
 
     private class MoveToNextListener implements AdapterView.OnItemClickListener {
