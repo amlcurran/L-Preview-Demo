2a2611d55769179d3123dbb79ca67263e311eb13
Alex Curran <aml.curran@gmail.com>
2014-11-15 13:53:57 +0000

    Clip fab to be circular

--- app/src/main/java/uk/co/amlcurran/lpreviewdemo/StateListAnimationActivity.java
+++ app/src/main/java/uk/co/amlcurran/lpreviewdemo/StateListAnimationActivity.java
@@ -1,7 +1,10 @@
 package uk.co.amlcurran.lpreviewdemo;
 
 import android.app.Activity;
+import android.graphics.Outline;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewOutlineProvider;
 
 public class StateListAnimationActivity extends Activity {
 
@@ -9,5 +12,13 @@ public class StateListAnimationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state_list);
+
+        findViewById(R.id.fab).setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setOval(0, 0, view.getWidth(), view.getHeight());
+            }
+        });
+        findViewById(R.id.fab).setClipToOutline(true);
     }
 }
